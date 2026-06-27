781bf8081f298fddeffa8da0e0f87865f2bcc3e3
Zenqor <102019853+zenq0r@users.noreply.github.com>
2026-06-27 22:24:43 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDJj1Ps8OQcrvJm18pIi9cGL14CmbZbzfM",
+    authDomain: "zenqor-crm.firebaseapp.com",
+    projectId: "zenqor-crm",
+    storageBucket: "zenqor-crm.firebasestorage.app",
+    messagingSenderId: "1056847970162",
+    appId: "1:1056847970162:web:36592162bad31ed54967eb"
+};
