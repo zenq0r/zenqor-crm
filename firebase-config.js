61305ebf854cb35288a2586176f2844b8b93cf8d
Zenqor <102019853+zenq0r@users.noreply.github.com>
2026-06-27 22:21:21 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// fail: firebase-config.js
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDJj1Ps8OQcrvJm18pIi9cGL14CmbZbzfM",
+    authDomain: "zenqor-crm.firebaseapp.com",
+    projectId: "zenqor-crm",
+    storageBucket: "zenqor-crm.firebasestorage.app",
+    messagingSenderId: "1056847970162",
+    appId: "1:1056847970162:web:36592162bad31ed54967eb"
 };
